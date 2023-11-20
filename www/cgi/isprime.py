f0c6c7d8c199a282ec07b12a84ef4c71045ac57d
baki <badrkhatib@hotmail.com>
2023-11-20 20:35:25 +0100

    added POST part - not done yet

--- www/cgi/isprime.py
+++ www/cgi/isprime.py
@@ -2,29 +2,33 @@
 
 import os
 
-value = os.environ["value"]
-
-def is_prime(n):
-	if n < 2:
-		return(False)
-	i = 2
-	while i * i <= n:
-		if n % i == 0:
+method = os.environ["REQUEST_METHOD"]
+if method == "GET":
+	value = os.environ["value"]
+	def is_prime(n):
+		if n < 2:
 			return(False)
-		i += 1
-	return(True)
+		i = 2
+		while i * i <= n:
+			if n % i == 0:
+				return(False)
+			i += 1
+		return(True)
 
-if value:
-	try:
-		num = int(value)
-		if is_prime(num):
-			r = f"<output>{num} est un nombre premier :)</output>"
-		else:
-			r = f"<output>{num} n'est pas un nombre premier :)</output>"
-	except ValueError:
-		r = "Veuillez entrer un nombre entier valide."
-else:
-	r = "Veuillez soumettre un formulaire avec une valeur."
+	if value:
+		try:
+			num = int(value)
+			if is_prime(num):
+				r = f"<output>{num} est un nombre premier :)</output>"
+			else:
+				r = f"<output>{num} n'est pas un nombre premier :)</output>"
+		except ValueError:
+			r = "Veuillez entrer un nombre entier valide."
+	else:
+		r = "Veuillez soumettre un formulaire avec une valeur."
+elif method == "POST":
+	r = "TEST"
+
 
 result = f"""
 <!DOCTYPE html>
